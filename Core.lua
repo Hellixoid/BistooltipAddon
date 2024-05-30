411e757b56557bd5b24634ec12a42d86f1a63a46
Hellixoid <Hellixoid@gmail.com>
2024-05-30 14:22:10 +0300

    v8.01

--- Core.lua
+++ Core.lua
@@ -40,7 +40,7 @@ end
 function BistooltipAddon:OnInitialize()
     createEquipmentWatcher()
     BistooltipAddon.AceAddonName = "Bis-Tooltip"
-    BistooltipAddon.AddonNameAndVersion = "Bis-Tooltip v8.0"
+    BistooltipAddon.AddonNameAndVersion = "Bis-Tooltip v8.01"
     BistooltipAddon:initConfig()
     BistooltipAddon:addMapIcon()
     BistooltipAddon:initBislists()
